92159cfa3c5139356bf0e038d1b156c9bf58c025
Nguyen Van Trong <trongnv130@ghtk.co>
2025-07-26 17:08:45 +0700

    Feat: Release RAG-E bot ver2

--- rag/streamlit_app.py
+++ rag/streamlit_app.py
@@ -1,7 +1,8 @@
 import os
 import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(current_dir)
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
 import requests
 import streamlit as st
